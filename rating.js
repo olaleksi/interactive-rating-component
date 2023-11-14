0cf541dbf96f493a5d8d98422335aae1837beedd
Olalekan olajide <72236651+olaleksi@users.noreply.github.com>
2023-11-14 15:35:34 +0100

    uploaded some files

---
+++ rating.js
@@ -0,0 +1,51 @@
+const ratingMenu = document.querySelector(".rating-menu");
+const successContainer = document.querySelector(".success-container");
+const submitButton = document.getElementById("submit-btn");
+const rateAgain = document.getElementById("rate-again");
+const ratings = document.querySelectorAll(".btn");
+const ratingNumber = document.getElementById("rating-number");
+
+submitButton.addEventListener("click", () => {
+  ratingMenu.classList.add('hidden');
+  successContainer.classList.remove("hidden");
+  ratings.forEach((rating) => {
+    rating.addEventListener("click", () => {
+      ratingNumber.innerHTML = rating.innerHTML;
+    });
+  });
+});
+
+rateAgain.addEventListener("click", () => {
+  ratingMenu.classList.remove('hidden');
+  successContainer.classList.add("hidden");
+}); 
+
+
+// Get all the buttons into a node list
+//let buttons = document.querySelectorAll(".btn");
+
+// Set an event handler on the document so that when
+// any element is clicked, the event will bubble up to it
+document.addEventListener("click", function(evt){
+    // Check to see if it was a button that was clicked
+    if(evt.target.classList.contains("btn")){
+        // Loop over all the buttons & remove the active class
+        ratings.forEach(function(rating){
+            rating.classList.remove("active")
+        });
+         // Make the clicked button have the active class
+        evt.target.classList.add("active")
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
